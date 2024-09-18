fcbe02c0422b0e4a196cc8dad35215d78aecfae4
Vitor Pimenta <vitor.pimenta@mouts.info>
2024-09-17 21:37:58 -0300

    Considera null na verificacao de versoes

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -24,7 +24,7 @@ class Dashboard extends Page
 
     public array $chamadosInternos;
     public array $chamadosExternos;
-    public array $versao;
+    public ?array $versao;
     public array $clientesImplantacao;
     public array $chamadosDiasSemana;
 
