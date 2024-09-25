edbdb1f591765a4e877d5e7dab4ca7fcceaa8ee5
Vitor Pimenta <vitor.pimenta@mouts.info>
2024-09-25 00:33:51 -0300

    Teste de notificacoes agendadas

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Console\Commands\GerarNotificacoesGerais;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('app:gerar-notificacoes-gerais', function () {
+    $not = new GerarNotificacoesGerais();
+    $not->handle();
+})->purpose('Gera notificacao geral')->everyMinute();
